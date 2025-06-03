d12f3b2f7186343279c0aa55ac718b90d6b61603
programmerShinobi <faqihpratamamuhti@gmail.com>
2025-06-04 02:10:11 +0700

    [update] repository & services : showing Todolist

---
+++ src/service/TodolistServiceImpl.java
@@ -0,0 +1,37 @@
+package service;
+
+import entity.Todolist;
+import repository.TodolistRepository;
+
+public class TodolistServiceImpl implements TodolistService {
+
+	private final TodolistRepository todolistRepository;
+
+	public TodolistServiceImpl(TodolistRepository todolistRepository) {
+		this.todolistRepository = todolistRepository;
+	}
+
+	@Override
+	public void showTodolist() {
+		Todolist[] model = todolistRepository.getAll();
+		System.out.println("TODOLIST");
+		for (int i = 0; i < model.length; i++) {
+			String todo = String.valueOf(model[i]);
+			int number = i + 1;
+
+			if (todo != null) {
+				System.out.println(number + ". " + todo);
+			}
+		}
+	}
+
+	@Override
+	public void addTodolist(String todo) {
+
+	}
+
+	@Override
+	public void removeTodolist(Integer number) {
+
+	}
+}
